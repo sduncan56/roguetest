2e4279ddb19f97a9faa6df9ed16c0dd8bbe755a4
Shane <shane.duncan56@gmail.com>
2019-10-21 21:31:08 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "libtcod/libtcod.hpp"
+int main()
+{
+    TCODConsole::initRoot(80,50,"libtcod test", false);
+    while(!TCODConsole::isWindowClosed())
+    {
+        TCOD_key_t key;
+        TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS
+        ,&key, NULL);
+        TCODConsole::root->clear();
+        TCODConsole::root->putChar(40, 25, '@');
+        TCODConsole::flush();
+    }
+    return 0;
+}
